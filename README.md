15b0dd722de696ba2400ac819ef27a8c14934e81
pmcarpan <arpan0123@gmail.com>
2020-07-27 20:58:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Take a break!
+
+Simple python script to pester you every few seconds/minutes/hours or so by opening an url in the browser.
+
+Proven to be effective against extension of the medial region, javascript fatigue, back-ache, etc.
+
+Use `python main.py -h` for help.
+
+##### Just in case you are too lazy to check the help
+
+```
+usage: main.py [-h] [-m | -H] interval [url]
+
+positional arguments:
+  interval       Disturb me after this much time (in seconds by default)
+  url            I like this url (open this in the browser)
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -m, --minutes  Seconds? I want minutes!
+  -H, --hours    Minutes? Pfft! Hours, or nothing
+```
---
+++ main.py
@@ -0,0 +1,51 @@
+import argparse
+import time
+import webbrowser
+
+
+DEFAULT_URL = "https://www.youtube.com/watch?v=D7ab595h0AU"
+
+"""
+Very low intervals may lead to unintended consequences like crashing/freezing/etc.
+Prevent the interval from going below this value (in seconds). Change at your own risk!
+"""
+SPAM_THRESHOLD = 10
+
+
+def take_a_break(interval_in_seconds, url):
+    while True:
+        time.sleep(interval_in_seconds)
+        webbrowser.open_new(url)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("interval", type=float, help="Disturb me after this much time (in seconds by default)")
+    parser.add_argument("url", nargs="?", default=DEFAULT_URL, help="I like this url (open this in the browser)")
+    
+    units = parser.add_mutually_exclusive_group()
+    units.add_argument("-m", "--minutes", action="store_true", help="Seconds? I want minutes!")
+    units.add_argument("-H", "--hours", action="store_true", help="Minutes? Pfft! Hours, or nothing")
+
+    args = parser.parse_args()
+    interval_in_seconds = args.interval
+
+    if interval_in_seconds < 0:
+        raise ValueError("Negative interval not allowed. I don't have a time machine, you know.")
+
+    if args.minutes:
+        interval_in_seconds *= 60
+    elif args.hours:
+        interval_in_seconds *= 3600
+
+    if interval_in_seconds < SPAM_THRESHOLD:
+        raise ValueError("This interval seems too low, and may cause crashes/freezes/etc.")
+
+    try:
+        take_a_break(interval_in_seconds, args.url)
+    except KeyboardInterrupt:
+        print("Thanks for trying this! Good night/day/whatever.")
+
+
+if __name__ == "__main__":
+    main()
